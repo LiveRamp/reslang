d3469354860d6c8877766a2c020f150b202b0db1
Dylan Quenneville <dylan.quenneville@liveramp.com>
2019-09-17 18:51:04 +0800

    Regenerate test data uses yarn script

--- regenerate-test-data.sh
+++ regenerate-test-data.sh
@@ -4,10 +4,10 @@ apis=(complex-resource direct2dist distribution file request simple-resource sin
 
 for api in "${apis[@]}"
 do
-   : 
+   :
    echo Updating test data for ${api}
-    ./reslang ./models/${api} --stdout > ./models/${api}/swagger.expected
-    ./reslang ./models/${api} --stdout --parsed > ./models/${api}/parsed.expected
+    yarn --silent reslang ./models/${api} --stdout > ./models/${api}/swagger.expected
+    yarn --silent reslang ./models/${api} --stdout --parsed > ./models/${api}/parsed.expected
 done
 
 echo Updated test data
