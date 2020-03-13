190425caddb86f4ef5891d40f726a586e9bb9a8a
Bryan Eslinger <bryan.eslinger@liveramp.com>
2020-03-12 21:06:15 -0700

    v1.1.1-rc.3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import cli, { parseArguments } from "./cli"
 
+// TODO: expose a strict/"production" mode enforcing proper parsing of the version
 ((version: string | undefined) => {
   cli(parseArguments(), version || "UNKNOWN")
 })(process.env.npm_package_version)
