da4af76b37522f85db099875e6ae870efdd9a682
Andrew McVeigh <andrew.x.mcveigh@gmail.com>
2020-04-15 15:14:39 -0700

    added script to update railroad diagrams

--- docs/bnf/generate-railroads.sh
+++ docs/bnf/generate-railroads.sh
@@ -5,5 +5,14 @@
 cat ../../src/grammar/*.pegjs > grammar.pegjs
 
 grammkit grammar.pegjs
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+  --headless \
+  --disable-gpu \
+  --no-margins \
+  --print-to-pdf grammar.html
+  mv output.pdf railroad-for-reslang.pdf
+
 # remove the evidence ;-P
 rm grammar.pegjs
+rm grammar.html
