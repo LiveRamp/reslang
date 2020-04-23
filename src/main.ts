e29ec59f30ccabc956745ba753db9b16a5745da9
Andrew McVeigh <andrew.x.mcveigh@gmail.com>
2020-04-23 15:45:05 -0700

    added matt's syntax highlighting extension for vscode

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ import { IRules } from "./rules"
 const RULES = "rules.json"
 const LOCAL_RULES = lpath.join(__dirname, "library", RULES)
 
-export const VERSION = "v1.4.1"
+export const VERSION = "v1.4.2"
 
 // parse the cmd line
 const args = yargs
