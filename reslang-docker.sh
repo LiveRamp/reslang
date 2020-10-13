ea9c486e90aba520fe731bf6369cee2603f8bf96
CJ Apel <cjapel@liveramp.com>
2020-10-13 15:30:47 -0700

    Update reslang-docker
    Remove unnecessary logs and errors, pass user-specified options, and
    give it a "usage" function.

--- reslang-docker.sh
+++ reslang-docker.sh
@@ -2,13 +2,50 @@
 
 set -eu
 
-echo "This only accepts absolute paths e.g. /User/a/b/models/" >&2
+RESLANG_IMG=gcr.io/liveramp-eng/reslang:master
 
-FULL_PATH=$1
-echo "Reslang files to read: $FULL_PATH" >&2
+usage() {
+  cat <<USAGE
 
-DIR_NAME=$(basename "$FULL_PATH")
-SRC_PATH="/app/reslang/${DIR_NAME}"
-echo ${SRC_PATH} >&2
+  Usage: run a Reslang container image on a specified directory.
 
-docker run -v "${FULL_PATH}:${SRC_PATH}" gcr.io/liveramp-eng/reslang:master ${SRC_PATH} --stdout
+  $ $0 [ RESLANG_DIR ] [ OPTS ...]
+
+  Examples:
+
+  $ $0 /abs/path/to/reslang_directory
+  $ $0 /abs/path/to/reslang_directory --diagram diagram_name
+
+  OPTS: All options available to Reslang can be passed to this script.
+USAGE
+
+exit "$1"
+}
+
+# is_absolute_path indicates if a string is prefixed with a leading slash.
+# It returns a 0 (success) code if the path is absolute, else 1 (error).
+#   $1 : string
+is_absolute_path() {
+  case "$1" in
+    /*) : 0 ;;
+    *) : 1 ;;
+  esac
+
+  return "$_"
+}
+
+main() {
+  local host_path; local mount_path;
+  host_path="$1" ; shift ;
+  mount_path=/app/reslang/"$(basename "$host_path")"
+
+  if ! is_absolute_path "$host_path"; then
+    printf "error: expected absolute path, got %s\n" "$host_path"
+    usage 1
+  fi
+
+  docker run -v "${host_path}:${mount_path}" "$RESLANG_IMG" \
+    "${mount_path}" --stdout "$@"
+}
+
+main "$@"
