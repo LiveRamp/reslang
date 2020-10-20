dd242f8b026d32d575e1ea5863330c6fa99ae193
CJ Apel <cjapel@liveramp.com>
2020-10-20 13:44:47 -0700

    Update reslang-docker (#140)
    * Update reslang-docker

--- reslang-docker.sh
+++ reslang-docker.sh
@@ -2,13 +2,59 @@
 
 set -eu
 
-echo "This only accepts absolute paths e.g. /User/a/b/models/" >&2
+RESLANG_IMG=gcr.io/liveramp-eng/reslang:master
 
-FULL_PATH=$1
-echo "Reslang files to read: $FULL_PATH" >&2
+usage() {
+  cat <<USAGE
 
-DIR_NAME=$(basename "$FULL_PATH")
-SRC_PATH="/app/reslang/${DIR_NAME}"
-echo ${SRC_PATH} >&2
+  Usage: run a Reslang container image on a specified directory.
 
-docker run -v "${FULL_PATH}:${SRC_PATH}" gcr.io/liveramp-eng/reslang:master ${SRC_PATH} --stdout
+  $ $0 RESLANG_DIR [ OPTS ...]
+
+  Examples:
+
+  $ $0 /abs/path/to/reslang_directory
+  $ $0 /abs/path/to/reslang_directory --diagram diagram_name
+
+  OPTS: The following options will **not** work with this script:
+
+    --web, --open, --stripped, --testwrite, --testdir
+
+  To use those options, please run reslang un-containerized.
+USAGE
+
+exit "$1"
+}
+
+# is_absolute_path indicates if a string is prefixed with a leading slash.
+# It returns a 0 (success) code if the path is absolute, else 1 (error).
+#   $1 : string
+is_absolute_path() {
+  case "$1" in
+    /*) return 0 ;;
+     *) return 1 ;;
+  esac
+}
+
+# Run a reslang container on a user-specified directory.
+# NOTE: The reslang directory's parent is mounted into the container, so that
+# it can access imported resources from peers. In reslang, all imports refer
+# to peer directories.
+main() {
+  if [ "$#" -lt 1 ]; then usage 1 ; fi
+
+  local host_path; local workspace;
+  host_path="$1" ; shift ;
+  parent_dir="$(dirname "$host_path")"
+  workspace=/app/reslang/workspace/
+
+  if ! is_absolute_path "$host_path"; then
+    printf "error: expected absolute path, got %s\n" "$host_path"
+    usage 1
+  fi
+
+  docker run -v "$parent_dir":"$workspace" "$RESLANG_IMG" \
+    "$workspace"/"$(basename "$host_path")" --stdout "$@"
+}
+
+main "$@"
