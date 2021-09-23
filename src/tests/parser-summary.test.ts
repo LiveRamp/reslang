2ada48104109e31de4718bda268e1425ea149f0f
Ezequiel Rosas <ezequiel.rosas@liveramp.com>
2021-09-23 17:03:00 -0500

    include basic unit tests

---
+++ src/tests/parser-summary.test.ts
@@ -0,0 +1,67 @@
+import { loadParser, clean } from "../parse"
+
+/** parse the reslang files and check that the correct
+ * abstract syntax tree is being generated
+ */
+describe("reslang summary parsing tests", () => {
+    let parser = loadParser();
+
+    function expected(...operations: any[]) {
+        return [{
+            category: "definition",
+            type: "resource",
+            kind: "resource-like",
+            operations: operations,
+            future: false,
+            parents: [],
+            short: "test",
+            singleton: false
+        }];
+    }
+    function operation(operation: string, summary: string, comment?: string) {
+        return {
+            summary: summary,
+            operation: operation,
+            comment: comment,
+            options: [],
+            errors: []
+        };
+    }
+
+    test("operations should include empty summary by default", () => {
+        let tree = clean(parser.parse(
+            `resource test {
+                /operations
+                POST
+            }`, {output: "parser"}));
+        expect(tree).toEqual(expected(operation("POST", "")))
+    })
+    test("operations may include only a description", () => {
+        let tree = clean(parser.parse(
+            `resource test {
+                /operations
+                "my description"
+                POST
+            }`, {output: "parser"}));
+        expect(tree).toEqual(expected(operation("POST", "", "my description")))
+    })
+    test("operations may include only a summary", () => {
+        let tree = clean(parser.parse(
+            `resource test {
+                /operations
+                "Summary: my summary"
+                POST
+            }`, {output: "parser"}));
+        expect(tree).toEqual(expected(operation("POST", "my summary", "")))
+    })
+    test("operations may include both summary and description", () => {
+        let tree = clean(parser.parse(
+            `resource test {
+                /operations
+                "Summary: my summary
+                my description"
+                POST
+            }`, {output: "parser"}));
+        expect(tree).toEqual(expected(operation("POST", "my summary", "my description")))
+    })
+})
