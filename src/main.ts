a42563a3cb8f5706de260274f11c1c1bcbceae69
Ezequiel Rosas <ezequiel.rosas@liveramp.com>
2021-11-01 15:02:44 -0600

    bump version

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ import JsonSchemaGen from "./genjsonschema"
 const RULES = "rules.json"
 const LOCAL_RULES = lpath.join(__dirname, "library", RULES)
 
-export const VERSION = "v6.1.4"
+export const VERSION = "v6.1.5"
 
 // parse the cmd line
 const args = yargs
