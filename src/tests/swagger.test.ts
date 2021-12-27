388a47a30906039ec7452670c362c3352ea9eec4
Ezequiel Rosas <ezequiel.rosas@liveramp.com>
2021-12-27 16:07:02 -0600

    include oneof test

--- src/tests/swagger.test.ts
+++ src/tests/swagger.test.ts
@@ -9,19 +9,26 @@ describe("swagger generation tests", () => {
     test.each(allModels)("swagger(%s)", (a) => {
         compare(a)
     })
+
+    test("swagger oneof polymorphism", () => {
+        compare("polymorphism", true, "oneof.expected")
+    })
 })
 
 /** compare the output with saved swagger */
-function compare(module: string) {
+function compare(module: string, oneofPolymorphism = false, expectedPath = "swagger.expected") {
     const swag = new SwagGen(
         [`models/${module}`],
         { ignoreRules: true, pagination: "cursor", limit: 10, maxLimit: 100 },
         "PROD",
         "",
-        true
+        true,
+        false,
+        true,
+        oneofPolymorphism
     )
     const got = swag.generate();
-    const expected = yaml.load(readFile(`models/${module}/testdata/swagger.expected`));
+    const expected = yaml.load(readFile(`models/${module}/testdata/${expectedPath}`));
 
     expect(yaml.dump(clean(got), {noRefs: true}))
         .toStrictEqual(yaml.dump(clean(expected), {noRefs: true}))
